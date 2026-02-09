9437ad6990f2d917ed65c6985c5cabea599e5309
Sebas2702aaa <sebas2702tito@gmail.com>
2026-02-09 11:06:10 -0600

    Primer commit

---
+++ src/test/java/com/tienda/Desarollo/TiendaDesarrolloApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.Desarrollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaDesarrolloApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
